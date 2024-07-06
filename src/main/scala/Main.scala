126ff2e3c76089e426f5f8d19e857ad15c0ca53f
miueon <cruxbell@gmail.com>
2024-07-06 17:59:30 +0800

    using eff

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,213 +1,107 @@
+import cats.Defer
+import cats.Eval
 import cats.Monad
 import cats.MonadThrow
+import cats.data.EitherT
 import cats.data.Kleisli
+import cats.data.Reader
 import cats.data.State
+import cats.data.StateT
 import cats.syntax.all.*
 import effect.*
+import effect.*
+import effect.TaskEffect.*
+import effect.TaskInterpretation.*
 import effect.pull.Stream
+import org.atnos.eff.*
+import org.atnos.eff.all.*
+import org.atnos.eff.syntax.all.*
+import rawmode.*
 import rawmode.all.*
+import rawmode.all.disableRawMode as resetRawMode
+import rawmode.all.enableRawMode as setRawMode
+import util.Utils.*
 
 import java.nio.charset.Charset
 import java.util.concurrent.Executors
 import scala.scalanative.libc.{errno as libcErrno, *}
+import scala.scalanative.posix.cpio
+import scala.scalanative.posix.errno
 import scala.scalanative.posix.termios
 import scala.scalanative.posix.unistd
 import scala.scalanative.unsafe.*
 import scala.scalanative.unsafe.Tag.USize
 import scala.scalanative.unsigned.UInt
 import scala.util.Try
-import rawmode.all.{disableRawMode as resetRawMode, enableRawMode as setRawMode}
-import cats.Eval
-import rawmode.*
-import scala.scalanative.posix.cpio
-import scala.scalanative.posix.errno
-import cats.data.EitherT
-import util.Utils.*
-import cats.data.StateT
-import cats.Defer
-import org.atnos.eff.*
-import org.atnos.eff.all.*
-import org.atnos.eff.syntax.all.*
-import effect.*
-import effect.TaskEffect.*
-import effect.TaskInterpretation.*
-import cats.data.Reader
-import Main.Res.enableR
 
-// val program: Eff[AppStack, Unit] =
-//   ???
-// bracket(TermIOS.enableRawMode)(t =>
-//   for
-//     // _ <- enableRawMode[AppStack]
-//     str <- testGetLine[AppStack]
-//     _ <- testPrintLine[AppStack](str)
-//   // _ <- disableRawMode[AppStack]
-//   yield ()
-// )(TermIOS.disableRawMode(_))
-
-// trait AlgInterp[F[_]: MonadThrow]:
-//   def enableRawMode: F[Unit]
-//   def readLine(): F[Option[String]]
-//   def printLine(str: String): F[Unit]
-//   def test(): F[Unit]
-
-// type Alg[F[_], A] = Kleisli[F, AlgInterp[F], A]
-
-// def readLine[F[_]: Monad]: Alg[F, Option[String]] = Kleisli(_.readLine())
-
-// def printLine[F[_]: Monad](str: String): Alg[F, Unit] = Kleisli(
-//   _.printLine(str)
-// )
-
-// def enableRawMode[F[_]: Monad]: Alg[F, Unit] = Kleisli(_.enableRawMode)
-
-// def test[F[_]: Monad]: Alg[F, Unit] =
-//   // Kleisli(_.test())
-//   for
-//     env <- Kleisli.ask[F, AlgInterp[F]]
-//     res <- Kleisli.liftF(env.test())
-//   yield res
-// def readCLine() =
-//   val buffer = stackalloc[Byte](1024)
-//   val line = stdio.fgets(buffer, 1024, stdio.stdin)
-//   fromCString(line)
-// val interp = new AlgInterp[Task]:
-//   def printLine(str: String): Task[Unit] =
-//     // var cstr = Zone(toCString(str))
-//     Task
-//       .delay {
-//         stdio.printf(c"%s\n", Zone(toCString(str)))
-//         // throw java.lang.RuntimeException("test")
-//       }
-//       .handleError(e => println(e.getMessage))
-//       .void
-//     // Zone {
-//     //   IO.fork(stdio.printf(c"%s\n", toCString(str)))
-//     // }
-//     // IO.forkUnit(println(str))
-//   def readLine(): Task[Option[String]] =
-//     // IO(Try(readCLine()).toOption)
-//     Task.forkUnit(readCLine().some)
-
-//   def test(): Task[Unit] =
-//     val c = scala.io.StdIn.readChar()
-//     if c != 'q' then Task(println((c).toChar)) >> test()
-//     else Task.unit
-
-//   import TerminOSOps.*
-//   def enableRawMode: Task[Unit] =
-//     Task
-//       .now {
-
-//       }
-//       .flatMap(_ => test())
-
-// def program[F[_]: Monad]: Alg[F, Unit] =
-//   for
-//     _ <- printLine("What's your name?")
-//     _ <- enableRawMode
-//     _ <- test
-//   // name <- readLine
-//   // _ <- printLine(s"Hello $name")
-//   yield ()
 case class EditorConfig(a: Int)
 
 type EditorConfigState[F[_]] = StateT[F, EditorConfig, Unit]
+type EitherRawResult[A] = Either[Int, A]
+
+type _errRaw[R] = EitherRawResult |= R
 
 object Main extends IOApp:
   inline def ctrlKey(c: CChar): CChar = (c & 0x1f).toByte
-  inline def resetCursorAndScreen(): Task[Unit] =
-    Task(unistd.write(unistd.STDOUT_FILENO, c"\x1b[2J", 4.toUInt)) // reset screen
-      >> Task(unistd.write(unistd.STDOUT_FILENO, c"\x1b[H", 3.toUInt)) // reset cursor, [x,xH
+  inline def resetCursorAndScreen[F[_]: MonadThrow: Defer](): F[Unit] =
+    Defer[F].defer {
+      unistd.write(unistd.STDOUT_FILENO, c"\x1b[2J", 4.toUInt).pure // reset screen
+        >> unistd.write(unistd.STDOUT_FILENO, c"\x1b[H", 3.toUInt).pure // reset cursor, [x,xH
+    }.void
 
-  def editorReadKey(ref: Ref[Task, Ptr[CChar]]): Task[Unit] =
+  def editorReadKey[F[_]: MonadThrow: Defer](ref: Ref[F, Ptr[CChar]]): F[Unit] =
     for
       cPtr <- ref.get
-      nread <- Task.delay(unistd.read(unistd.STDIN_FILENO, cPtr, UInt.valueOf(1)))
+      nread <- Defer[F].defer(unistd.read(unistd.STDIN_FILENO, cPtr, UInt.valueOf(1)).pure)
       _ <-
         if nread != 1 then editorReadKey(ref)
-        else if nread == -1 && errno.errno != errno.EAGAIN then Task.raiseError(new Exception("read"))
-        else Task.unit
+        else if nread == -1 && errno.errno != errno.EAGAIN then new Exception("read").raiseError
+        else MonadThrow[F].unit
     yield ()
 
-  def editorProcessKeypress(): EitherT[Task, Int, Unit] =
-    val result: Task[Either[Int, Unit]] = for
-      cPtrRef <- Task(Ref[Task, Ptr[CChar]](malloc[CChar]))
-      _ <- editorReadKey(cPtrRef)
+  def editorProcessKeypress[R: _task](): Eff[R, EitherRawResult[Unit]] =
+    for
+      cPtrRef <- fromTask(Ref[Task, Ptr[CChar]](malloc[CChar]).pure)
+      _ <- fromTask(editorReadKey(cPtrRef))
       // r <- Task(println("keypressed")) >> Task(Left(0))
-      cPtr <- cPtrRef.get
+      cPtr <- fromTask(cPtrRef.get)
       // _ <- Task(println(s"keypressed: ${!cPtr}"))
       r <- !cPtr match
-        case a if a == ctrlKey('q') => resetCursorAndScreen() >> Task(Left(0))
-        case _                      => Task(Right(()))
-    yield r
-    EitherT(result)
-
-  def editorDrawRows(): Task[Unit] =
-    Stream.eval(Task(unistd.write(unistd.STDOUT_FILENO, c"~\r\n", 3.toUInt)).void).repeat.take(24).run
-
-  def editorRefreshScreen(): Task[Unit] =
-    for
-      _ <- resetCursorAndScreen()
-      _ <- editorDrawRows()
-      _ <- Task(unistd.write(unistd.STDOUT_FILENO, c"\x1b[H", 3.toUInt))
-    yield ()
-
-  def rawmodeResource[F[_]: MonadThrow: Defer]: F[Resource[F, TermIOS]] =
-    Resource.make(TermIOS.enableRawMode[F])(TermIOS.disableRawMode[F]).pure
-
-  type _readerInt[R] = MemberIn[Reader[Int, *], R]
-  type _acc[R] = MemberIn[State[Int, *], R]
-
-  def getProperty[R: _readerInt: _acc]: Eff[R, Int] =
-    for
-      r <- ask[R, Int]
-      _ <- modify((_: Int) + r)
+        case a if a == ctrlKey('q') => fromTask(resetCursorAndScreen[Task]() >> Task(Left(0)))
+        case _                      => fromTask(Task(Right(())))
     yield r
 
-  object Res:
-    def enableR[R: _safe]: Eff[R, Unit] =
-      protect {
-        println("enable R")
-      }
+  def editorDrawRows[F[_]: MonadThrow: Defer](): F[Unit] =
+    Stream.eval(Defer[F].defer(unistd.write(unistd.STDOUT_FILENO, c"~\r\n", 3.toUInt).pure).void).repeat.take(24).run
 
-    def disableR[R: _safe]: Eff[R, Unit] =
-      protect {
-        println("disable R")
-      }
-
-    def usingIt[R:  _task]: Eff[R, Unit] =
-      fromTask(Task(println("using it")))
-
-  def program[R: _Safe: _task]: Eff[R, Unit] =
+  def editorRefreshScreen[R: _task](): Eff[R, Unit] =
     for
-      _ <- bracket(Res.enableR)(_ => Res.usingIt)(_ => Res.disableR)
-      _ <- fromTask(Task(println("test")))
-    // rawmodeResource <- fromTask(rawmodeResource)
-    // _ <- fromTask(rawmodeResource.use(_ =>
-
-    //   ???))
+      _ <- fromTask(resetCursorAndScreen())
+      _ <- fromTask(editorDrawRows())
+      _ <- fromTask(Task(unistd.write(unistd.STDOUT_FILENO, c"\x1b[H", 3.toUInt)))
     yield ()
+
+  type AppStack = Fx.fx1[Task]
+
+  def program: Eff[AppStack, Unit] =
+    def go(): Eff[AppStack, Unit] =
+      for
+        _ <- editorRefreshScreen[AppStack]()
+        r <- editorProcessKeypress[AppStack]()
+        _ <- if r.isLeft then Eff.pure(()) else go()
+      yield ()
+    go()
 
   def pureMain(args: List[String]): IO[Unit] =
-    program[Fx2[Safe, Task]].runSafe.toTask.void.asIO
-    // Resource
-    //   .make[Task, TermIOS](TermIOS.enableRawMode)(TermIOS.disableRawMode)
-    //   .use(_ =>
-    //     def go(): Task[Unit] =
-    //       for
-    //         _ <- editorRefreshScreen()
-    //         result <- editorProcessKeypress().isLeft
-    //         _ <- if result then Task.unit else go()
-    //       yield ()
-    //     go()
-    //   )
-    //   .handleErrorWith(e =>
-    //     resetCursorAndScreen() >> Task.apply(
-    //       Zone(stdio.printf(c"%s\n%s\n", toCString(e.getMessage), string.strerror(errno.errno)))
-    //     )
-    //   )
-    //   .asIO
-    //   .void
+    Resource
+      .make[Task, TermIOS](TermIOS.enableRawMode)(TermIOS.disableRawMode)
+      .use(_ => program.toTask)
+      .handleErrorWith(e =>
+        resetCursorAndScreen[Task]() >> Task.apply(
+          Zone(stdio.printf(c"%s\n%s\n", toCString(e.getMessage), string.strerror(errno.errno)))
+        )
+      )
+      .asIO
+      .void
   end pureMain
 end Main
